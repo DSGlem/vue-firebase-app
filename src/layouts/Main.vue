071984c2ab87c9b5bddc5578882b9f2941b7caad
DSGlem <davidsporysh@gmail.com>
2020-09-03 12:03:04 +0300

    layout was added

---
+++ src/layouts/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-100 d-flex flex-column">
+    <Header></Header>
+    <router-view class="flex-grow-1 flex-grow-0 flex-basis-auto" />
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+export default {
+  name: "Main",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
