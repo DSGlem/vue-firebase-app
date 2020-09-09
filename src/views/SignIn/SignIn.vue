7c71d1f680f2065d8607b65879f9593d672a1377
DSGlem <davidsporysh@gmail.com>
2020-09-09 10:16:19 +0300

    sign in, sign up views was updated

--- src/views/SignIn/SignIn.vue
+++ src/views/SignIn/SignIn.vue
@@ -1,91 +1,16 @@
 <template>
-  <div class="container d-flex flex-column  justify-content-center">
-    <div class="row">
-      <div class="col-4 offset-4">
-        <div class="">
-          <h4 class="text-center">Member Login</h4>
-        </div>
-        <div class="">
-          <form action="/examples/actions/confirmation.php" method="post">
-            <div class="form-group">
-              <input
-                type="text"
-                class="form-control"
-                name="username"
-                placeholder="Username"
-                required="required"
-              />
-            </div>
-            <div class="form-group">
-              <input
-                type="password"
-                class="form-control"
-                name="password"
-                placeholder="Password"
-                required="required"
-              />
-            </div>
-            <div class="form-group">
-              <button
-                type="submit"
-                class="btn btn-primary btn-lg btn-block login-btn"
-              >
-                Login
-              </button>
-            </div>
-          </form>
-        </div>
-        <div class="">
-          <a href="#">Forgot Password?</a>
-        </div>
-      </div>
+  <div class="conatine d-flex align-items-center justify-content-center">
+    <div class="col-4">
+      <SignIn></SignIn>
     </div>
   </div>
-
-  <!-- <div class="container d-flex flex-column  justify-content-center">
-    <h1 class="text-center">Sign in</h1>
-    <div class="row">
-      <div class="col-4 offset-4">
-        <form class="">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <small id="emailHelp" class="form-text text-muted"
-              >We'll never share your email with anyone else.</small
-            >
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div class="form-group form-check">
-            <input
-              type="checkbox"
-              class="form-check-input"
-              id="exampleCheck1"
-            />
-            <label class="form-check-label" for="exampleCheck1"
-              >Check me out</label
-            >
-          </div>
-          <button type="submit" class="btn btn-primary w-100">SignIn</button>
-        </form>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
+import SignIn from "@/components/Forms/SignIn";
 export default {
-  name: "SignIn"
+  components: {
+    SignIn
+  }
 };
 </script>
