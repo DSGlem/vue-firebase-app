397c8eebaee6853ccdb07a55d754b1f6e964a0d4
DSGlem <davidsporysh@gmail.com>
2020-09-09 11:15:52 +0300

    addingFilms component

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = new VueRouter({
     },
     {
       path: "/addCart",
-      component: () => import("../views/SignIn/SignIn.vue")
+      component: () => import("../views/Adding/AddingFilms.vue")
     },
     {
       path: "/SignIn",
---
+++ src/views/Adding/AddingFilms.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="conatine d-flex align-items-center justify-content-center">
+    <div class="col-4">
+      <AddingFilms></AddingFilms>
+    </div>
+  </div>
+</template>
+
+<script>
+import AddingFilms from "@/components/Forms/AddingFilms";
+export default {
+  components: {
+    AddingFilms
+  }
+};
+</script>
