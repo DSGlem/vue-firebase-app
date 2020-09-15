8ef857d8d7b68fba895565e41e9e40d53b0ca2e0
david-dsg <david.s@halo-lab.com>
2020-09-15 08:19:15 +0300

    challenge component

--- src/views/Challenge/Challenge.vue
+++ src/views/Challenge/Challenge.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="conatine d-flex align-items-center justify-content-center">
     <div class="col-4">
-      <AddingFilms></AddingFilms>
+      challenge
     </div>
   </div>
 </template>
 
 <script>
-import AddingFilms from "@/components/Forms/AddingFilms";
+// import AddingFilms from "@/components/Forms/AddingFilms";
 export default {
-  components: {
-    AddingFilms
-  }
+  components: {},
 };
 </script>
