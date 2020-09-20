d6841c7aa64f57773bd8fa0b7105266b6771dc76
MacBook <macbook@MacBook-Air-MacBook.local>
2020-09-20 12:03:38 +0300

    Sign Up form fix

--- src/components/Forms/SignUp.vue
+++ src/components/Forms/SignUp.vue
@@ -5,35 +5,100 @@
     </div>
     <div class="form-group">
       <input
-        class="form-control"
         type="text"
+        class="form-control"
+        name="firstName"
+        placeholder="First Name"
+        v-model.trim="$v.userData.firstName.$model"
+        :class="{
+          invalid: $v.userData.firstName.$error,
+        }"
+        required
+      />
+    </div>
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        name="lastName"
+        placeholder="Last Name"
+        required
+        v-model.trim="$v.userData.lastName.$model"
+        :class="{ invalid: $v.userData.lastName.$error }"
+      />
+    </div>
+    <div class="form-group">
+      <input
+        type="email"
+        class="form-control"
         name="email"
         placeholder="Email"
-        autocomplete="email"
         required
-        v-model="email"
+        v-model.trim="$v.userData.email.$model"
+        :class="{ invalid: $v.userData.email.$error }"
       />
     </div>
     <div class="form-group">
       <input
-        class="form-control"
         type="password"
+        class="form-control"
         name="password"
         placeholder="Password"
-        autocomplete="password"
         required
-        v-model="password"
+        v-model.trim="$v.userData.password.$model"
+        :class="{ invalid: $v.userData.password.$error }"
       />
     </div>
     <div class="form-group">
       <input
+        type="text"
         class="form-control"
-        type="password"
-        name="password-confirm"
-        placeholder="Confirm password"
-        autocomplete="password-confirm"
+        name="password"
+        placeholder="Date of Birth"
         required
-        v-model="passwordConfirm"
+        v-mask="'####-##-##'"
+        v-model.trim="$v.userData.dateOfBirth.$model"
+        :class="{ invalid: $v.userData.dateOfBirth.$error }"
+      />
+    </div>
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        name="password"
+        placeholder="Country of Residence"
+        required
+        v-model.trim="$v.userData.country.$model"
+        :class="{ invalid: $v.userData.country.$error }"
+      />
+      <input
+        type="text"
+        class="form-control"
+        name="password"
+        placeholder="Address"
+        required
+        v-model.trim="$v.userData.address.$model"
+        :class="{ invalid: $v.userData.address.$error }"
+      />
+      <input
+        type="number"
+        class="form-control"
+        name="password"
+        placeholder="Postcode"
+        required
+        v-model.trim="$v.userData.postcode.$model"
+        :class="{ invalid: $v.userData.postcode.$error }"
+      />
+    </div>
+    <div class="form-group">
+      <input
+        type="tel"
+        class="form-control"
+        name="password"
+        placeholder="Phone Number"
+        required
+        v-model="$v.userData.phone.$model"
+        :class="{ invalid: $v.userData.phone.$error }"
       />
     </div>
     <div class="form-group">
@@ -48,31 +113,50 @@
 </template>
 
 <script>
-// import firebase from "firebase";
+import { mask } from "vue-the-mask";
+import { required, email, minLength, numeric } from "vuelidate/lib/validators";
+import { validators } from "@/utils.js";
 export default {
-  data() {
-    return {
+  directives: { mask },
+  data: () => ({
+    userData: {
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
-      passwordConfirm: ""
-    };
+      phone: "",
+      dateOfBirth: "",
+      country: "",
+      address: "",
+      postcode: "",
+    },
+  }),
+  validations: {
+    userData: {
+      firstName: { validFormat: validators.onlyLetters, required },
+      lastName: { validFormat: validators.onlyLetters, required },
+      email: { email, required },
+      password: { minLength: minLength(6), required },
+      phone: { required },
+      dateOfBirth: { required },
+      country: { required },
+      address: { required },
+      postcode: { numeric, required },
+    },
   },
   methods: {
     async signUp() {
-      const formData = {
-        email: this.email,
-        password: this.password,
-        firstName: "firstName",
-        lastName: "lastName",
-        dateOfBirth: "dateOfBirth",
-        countryOfResidence: "countryOfResidence",
-        address: "address",
-        postcode: "postcode",
-        phone: "phone"
-      };
-
-      await this.$store.dispatch("signUp", formData);
-    }
-  }
+      this.$v.touch();
+      if (!this.$v.$invalid) {
+        await this.$store.dispatch("signUp", this.userData);
+        this.$router.push("/test");
+      }
+    },
+  },
 };
 </script>
+<style lang="scss" scoped>
+.invalid {
+  border: 1px solid red;
+}
+</style>
