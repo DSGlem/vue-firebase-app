a6fa75de7edb0ebb33d0d7845bcc062e1db58410
DSGlem <davidsporysh@gmail.com>
2020-09-21 08:22:37 +0300

    leadboard view

---
+++ src/views/Leadboard/Leadboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="bg-dark conatiner d-flex align-items-center justify-content-center"
+  >
+    <Leadboard />
+  </div>
+</template>
+
+<script>
+import Leadboard from "@/components/Leadboard/Leadboard";
+export default {
+  components: { Leadboard },
+};
+</script>
